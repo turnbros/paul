9d2717d4e4d29ec2da90ea9a68b652a19c123947
Dylan Turnbull <dylanturn@gmail.com>
2021-12-29 16:34:10 -0600

    make the workers go to the right place

--- src/workflows/server_count/__init__.py
+++ src/workflows/server_count/__init__.py
@@ -31,9 +31,16 @@ class Workflow(WorkflowStub):
         print(gs_count)
         return f"Howdy, {payload.get('name')}, the answer is {len(gs_count)}!"
 
+def get_temporal_ep():
+    kubernetes_host = os.getenv("KUBERNETES_SERVICE_HOST", False)
+    if kubernetes_host:
+        return "temporal-frontend.temporal.svc.cluster.local", 7233
+    else:
+        return "localhost", 7233
 
 async def worker_main():
-    client = WorkflowClient.new_client(namespace=NAMESPACE)
+    temporal_endpoint = get_temporal_ep()
+    client = WorkflowClient.new_client(host=temporal_endpoint[0], port=temporal_endpoint[1], namespace=NAMESPACE)
     factory = WorkerFactory(client, NAMESPACE)
     worker = factory.new_worker(TASK_QUEUE)
     worker.register_workflow_implementation_type(Workflow)
--- src/workflows/server_update/__init__.py
+++ src/workflows/server_update/__init__.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 from datetime import timedelta
-
+import os
 from temporal.workerfactory import WorkerFactory
 from temporal.workflow import workflow_method, Workflow, WorkflowClient
 
@@ -25,9 +25,16 @@ class Workflow(WorkflowStub):
     async def execute(self, payload):
         return f"Howdy, {payload.get('name')}, I'm gonna update some shit!"
 
+def get_temporal_ep():
+    kubernetes_host = os.getenv("KUBERNETES_SERVICE_HOST", False)
+    if kubernetes_host:
+        return "temporal-frontend.temporal.svc.cluster.local", 7233
+    else:
+        return "localhost", 7233
 
 async def worker_main():
-    client = WorkflowClient.new_client(namespace=NAMESPACE)
+    temporal_endpoint = get_temporal_ep()
+    client = WorkflowClient.new_client(host=temporal_endpoint[0], port=temporal_endpoint[1], namespace=NAMESPACE)
     factory = WorkerFactory(client, NAMESPACE)
     worker = factory.new_worker(TASK_QUEUE)
     worker.register_workflow_implementation_type(Workflow)
