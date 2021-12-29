fb0e119fe3d132b1ad6a074436bb3f10cf801470
Dylan Turnbull <dylanturn@gmail.com>
2021-12-29 16:25:24 -0600

    added something so that paul can find temporal

--- src/intent_routes.py
+++ src/intent_routes.py
@@ -1,3 +1,4 @@
+import os
 import asyncio
 import importlib.util
 from temporal.workflow import workflow_method, WorkflowClient
@@ -6,6 +7,12 @@ import importlib
 
 NAMESPACE = "default"
 
+def get_temporal_ep():
+    kubernetes_host = os.getenv("KUBERNETES_SERVICE_HOST", False)
+    if kubernetes_host:
+        return "temporal-frontend.temporal.svc.cluster.local", 7233
+    else:
+        return "localhost", 7233
 
 registered_intents = {}
 def register_intent_workflow(workflow_name):
@@ -15,7 +22,8 @@ def register_intent_workflow(workflow_name):
 async def execute_intent_workflow(intent_workflow, **kwargs):
     print(f"execute_intent_workflow: {intent_workflow}")
 
-    client = WorkflowClient.new_client(namespace=NAMESPACE)
+    temporal_endpoint = get_temporal_ep()
+    client = WorkflowClient.new_client(host=temporal_endpoint[0], port=temporal_endpoint[1], namespace=NAMESPACE)
     registered_workflow: registered_intents[intent_workflow].WorkflowStub = client.new_workflow_stub(registered_intents[intent_workflow].WorkflowStub)
     result = await registered_workflow.execute(kwargs)
     return result
