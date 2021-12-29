8154e34ee9495bf90b447814322ab4c0e2cc93b3
Dylan Turnbull <dylanturn@gmail.com>
2021-12-29 14:17:32 -0600

    fixed environment discovery

--- src/util/kubernetes.py
+++ src/util/kubernetes.py
@@ -3,30 +3,32 @@ from kubernetes import client, config
 import kubernetes
 from kubernetes.client.api.core_v1_api import CoreV1Api
 
-configmap_name = os.getenv("CONFIGMAP_NAME")
-k8s_endpoint = os.getenv("K8S_ENDPOINT")
-sa_mount_path = os.getenv("SA_MOUNT_PATH")
-configmap_name = os.getenv("CONFIGMAP_NAME")
+# configmap_name = os.getenv("CONFIGMAP_NAME")
+# k8s_endpoint = os.getenv("K8S_ENDPOINT")
+# sa_mount_path = os.getenv("SA_MOUNT_PATH")
+# configmap_name = os.getenv("CONFIGMAP_NAME")
 
 class Cluster:
 	def __init__(self, use_kubeconfig:bool = True):
 		
-		self.namespace = os.getenv("K8S_NAMESPACE")
-		if self.namespace is None:
-			# Read the namespace this deployment is n
-			with open(f"{sa_mount_path}/namespace") as namespace_file:
+		self.kubernetes_host = os.getenv("KUBERNETES_SERVICE_HOST", False)
+		self.sa_mount_path = "/run/secrets/kubernetes.io/serviceaccount"
+		self.namespace = None
+
+		if self.kubernetes_host:
+			with open(f"{self.sa_mount_path}/namespace") as namespace_file:
 				self.namespace = namespace_file.read()
 
-		# Configure the config or something
-		if not use_kubeconfig:
-			with open(f"{sa_mount_path}/token") as token_file:	
+			with open(f"{self.sa_mount_path}/token") as token_file:	
 				kube_config = client.Configuration()
 				kube_config.api_key["authorization"] = token_file.read()
 				kube_config.api_key_prefix['authorization'] = 'Bearer'
-				kube_config.host = k8s_endpoint
-				kube_config.ssl_ca_cert = f"{sa_mount_path}/ca.crt"
+				kube_config.host = self.kubernetes_host
+				kube_config.ssl_ca_cert = f"{self.sa_mount_path}/ca.crt"
 				self.kube_api = client.CoreV1Api(client.ApiClient(kube_config))
+
 		else:
+			# This is NOT running inside K8s
 			config.load_kube_config()
 			self._kube_api = client.CoreV1Api()
 
