e62df16aae1e38b102ae4e5dfcafc99fdd2bfc55
Dylan Turnbull <dylanturn@gmail.com>
2022-01-02 19:18:44 -0600

    broke things? idk..

--- src/app-workers.py
+++ src/app-workers.py
@@ -1,25 +1,51 @@
+import os
 import sys
-import time
 import asyncio
 import logging
 import importlib
-import threading
+from util import config
+from temporal.workflow import WorkflowClient
+from temporal.workerfactory import WorkerFactory
 
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 
 
-def start_worker(name):
-    logging.info(f"Starting worker {name}")
-    imported_worker = importlib.import_module(f"workflows.{name}")
-    worker_loop = asyncio.new_event_loop()
-    worker_loop.create_task(imported_worker.worker_main())
-    threading.Thread(name=f"{name}_worker", target=lambda: worker_loop.run_forever()).start()
-    logging.info(f"Worker {name} started!")
+async def start_worker(name):
+	logging.info(f"Starting worker {name}")
+	paul_config = config.Configuration()
+	worker_config = paul_config.read_workflow_config("server_update")
+	temporal_config = paul_config.read_temporal_config()
+
+	# Simple check to see if we're outside k8s
+	if os.getenv("KUBERNETES_SERVICE_HOST", False):
+		temporal_host = temporal_config.get("host")
+	else:
+		temporal_host = "localhost"    
+	temporal_port = temporal_config.get("port")
+
+	task_queue = worker_config.get("task_queue")
+	if task_queue is None:
+		raise Exception("Missing worker task_queue configuration!")
+
+	namespace = temporal_config.get("namespace")
+	if namespace is None:
+		raise Exception("Missing temporal namespace configuration!")
+
+	imported_worker = importlib.import_module(f"workflows.{name}")
+	client = WorkflowClient.new_client(host=temporal_host, port=temporal_port, namespace=namespace)
+	factory = WorkerFactory(client, namespace)
+	worker = factory.new_worker(task_queue)
+	worker.register_workflow_implementation_type(imported_worker.Workflow)	
+	factory.start()
+	logging.info("Worker started")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        start_worker(sys.argv[1])
-        while(True):
-            time.sleep(5)
+	if len(sys.argv) != 2:
+		print("too many args!")
+		exit()
+
+	worker_loop = asyncio.new_event_loop()
+	worker_loop.create_task(start_worker(sys.argv[1]))
+	worker_loop.run_forever()
--- src/workflows/server_update/__init__.py
+++ src/workflows/server_update/__init__.py
@@ -1,49 +1,64 @@
-import os
 import logging
-from util import kubernetes
-from util import config
-from temporal.workerfactory import WorkerFactory
-from temporal.workflow import workflow_method, Workflow, WorkflowClient
+from workflows.worker import TemporalWorker
 
 
 logging.basicConfig(level=logging.INFO)
 
 
-kube = kubernetes.Cluster()
-paul_config = config.Configuration()
-worker_config = paul_config.read_workflow_config("server_update")
-temporal_config = paul_config.read_temporal_config()
+class Workflow(TemporalWorker):
+    
+    name = "server_test"
 
+    async def worker_workflow(self, payload: dict):
+        logging.info("Log from worker_workflow")
+        return f"Hello, I'm gonna update some shit!....maybe"
 
-TASK_QUEUE = worker_config.get("task_queue")
-if TASK_QUEUE is None:
-    raise Exception("Missing worker task_queue configuration!")
-
-
-NAMESPACE = temporal_config.get("namespace")
-if NAMESPACE is None:
-    raise Exception("Missing temporal namespace configuration!")
-
-
-# Workflow Implementation
-class Workflow:
-    @workflow_method(task_queue=TASK_QUEUE)
-    async def execute(self, payload: dict):
-        return f"Howdy, {payload.get('name')}, I'm gonna update some shit!"
-
-
-async def worker_main():
-    # Simple check to see if we're outside k8s
-    if os.getenv("KUBERNETES_SERVICE_HOST", False):
-        temporal_host = temporal_config.get("host")
-    else:
-        temporal_host = "localhost"    
-
-    temporal_port = temporal_config.get("port")
-
-    client = WorkflowClient.new_client(host=temporal_host, port=temporal_port, namespace=NAMESPACE)
-    factory = WorkerFactory(client, NAMESPACE)
-    worker = factory.new_worker(TASK_QUEUE)
-    worker.register_workflow_implementation_type(Workflow)
-    factory.start()
-    logging.info("Worker started")
+#import os
+#import logging
+#from util import kubernetes
+#from util import config
+#from temporal.workerfactory import WorkerFactory
+#from temporal.workflow import workflow_method, Workflow, WorkflowClient
+#
+#
+#logging.basicConfig(level=logging.INFO)
+#
+#
+#kube = kubernetes.Cluster()
+#paul_config = config.Configuration()
+#worker_config = paul_config.read_workflow_config("server_update")
+#temporal_config = paul_config.read_temporal_config()
+#
+#
+#TASK_QUEUE = worker_config.get("task_queue")
+#if TASK_QUEUE is None:
+#    raise Exception("Missing worker task_queue configuration!")
+#
+#
+#NAMESPACE = temporal_config.get("namespace")
+#if NAMESPACE is None:
+#    raise Exception("Missing temporal namespace configuration!")
+#
+#
+## Workflow Implementation
+#class Workflow:
+#    @workflow_method(task_queue=TASK_QUEUE)
+#    async def execute(self, payload: dict):
+#        return f"Howdy, {payload.get('name')}, I'm gonna update some shit!"
+#
+#
+#async def worker_main():
+#    # Simple check to see if we're outside k8s
+#    if os.getenv("KUBERNETES_SERVICE_HOST", False):
+#        temporal_host = temporal_config.get("host")
+#    else:
+#        temporal_host = "localhost"    
+#
+#    temporal_port = temporal_config.get("port")
+#
+#    client = WorkflowClient.new_client(host=temporal_host, port=temporal_port, namespace=NAMESPACE)
+#    factory = WorkerFactory(client, NAMESPACE)
+#    worker = factory.new_worker(TASK_QUEUE)
+#    worker.register_workflow_implementation_type(Workflow)
+#    factory.start()
+#    logging.info("Worker started")
